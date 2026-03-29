d28fd34b6074275b5585d3ca90eed6a5ef872d9d
Abdullah x909 <abdullahrahat.ar@gmail.com>
2026-03-29 19:23:00 +0600

    Modify renamer.bat to sort images by creation time

--- tools/renamer.bat
+++ tools/renamer.bat
@@ -2,25 +2,39 @@
 setlocal enabledelayedexpansion
 
 echo ========================================
-echo    Renaming images by creation time...
+echo    Renaming images - OLDEST first
+echo    (Oldest photo = 1.jpg)
 echo ========================================
 
 set "folder=images"
 set count=1
 
+:: Go to the images folder
 cd /d "%~dp0%folder%" 2>nul || (
     echo ERROR: Folder "images" not found!
+    echo Make sure the "images" folder is in the same place as this .bat file.
     pause
     exit /b
 )
 
-:: Sort by date modified (newest first) or you can change to /O:D for oldest first
-for /f "delims=" %%F in ('dir /b /a-d /o:-d *.jpg *.jpeg *.png *.gif *.bmp *.webp 2^>nul') do (
-    ren "%%F" "!count!.jpg"
-    echo Renamed: %%F  --^>  !count!.jpg
-    set /a count+=1
+echo Processing...
+
+:: /O:D  = sort by date, oldest first
+:: /T:C  = use Creation time  (best for photos)
+:: You can change to /T:W if you prefer "Date Modified"
+
+for /f "delims=" %%F in ('dir /b /a-d /O:D /T:C *.jpg *.jpeg *.png *.gif *.bmp *.webp 2^>nul') do (
+    if exist "%%F" (
+        ren "%%F" "!count!.jpg"
+        echo Renamed: %%F  --^>  !count!.jpg
+        set /a count+=1
+    )
 )
 
 echo.
-echo Done! Total files renamed: !count!-1
+echo ========================================
+echo    Finished! Total files renamed: !count!-1
+echo    Oldest image is now 1.jpg
+echo ========================================
+
 pause
